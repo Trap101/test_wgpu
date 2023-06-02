401ad58602283576eeecd854cdd5e85ab22cd552
trap101 <cheng050929@gmail.com>
2023-06-02 16:26:01 +0100

    damn

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use log::debug;
 use naga::FastHashMap;
 use rustc_hash::FxHashMap;
-use wgpu::ShaderStages;
+use wgpu::{ShaderStages, include_wgsl};
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -89,59 +89,52 @@ impl State {
         };
         debug!("{:?}",&config);
         surface.configure(&device, &config);
-        // let frag = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        //     label: Some("frag"),
-        //     source: wgpu::ShaderSource::Glsl{shader:include_str!("frag1.frag").into(),stage:naga::ShaderStage::Fragment,defines:FastHashMap::default()},
-        // });        
-        // let vertex = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        //     label: Some("vertex"),
-        //     source: wgpu::ShaderSource::Glsl{shader:include_str!("vert1.vert").into(),stage:naga::ShaderStage::Vertex,defines:FastHashMap::default()},
-        // });        
+        let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));        
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
                 bind_group_layouts: &[],
                 push_constant_ranges: &[],
             });
-        // let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        //     label: Some("Render Pipeline"),
-        //     layout: Some(&render_pipeline_layout),
-        //     vertex: wgpu::VertexState {
-        //         module: &vertex,
-        //         entry_point: "main", // 1.
-        //         buffers: &[],           // 2.
-        //     },
-        //     fragment: Some(wgpu::FragmentState {
-        //         // 3.
-        //         module: &frag,
-        //         entry_point: "fs_main",
-        //         targets: &[Some(wgpu::ColorTargetState {
-        //             // 4.
-        //             format: config.format,
-        //             blend: Some(wgpu::BlendState::REPLACE),
-        //             write_mask: wgpu::ColorWrites::ALL,
-        //         })],
-        //     }),
-        //     primitive: wgpu::PrimitiveState {
-        //         topology: wgpu::PrimitiveTopology::TriangleList, // 1.
-        //         strip_index_format: None,
-        //         front_face: wgpu::FrontFace::Ccw, // 2.
-        //         cull_mode: Some(wgpu::Face::Back),
-        //         // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-        //         polygon_mode: wgpu::PolygonMode::Fill,
-        //         // Requires Features::DEPTH_CLIP_CONTROL
-        //         unclipped_depth: false,
-        //         // Requires Features::CONSERVATIVE_RASTERIZATION
-        //         conservative: false,
-        //     },
-        //     depth_stencil: None, // 1.
-        //     multisample: wgpu::MultisampleState {
-        //         count: 1,                         // 2.
-        //         mask: !0,                         // 3.
-        //         alpha_to_coverage_enabled: false, // 4.
-        //     },
-        //     multiview: None, // 5.
-        // });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1.
+                buffers: &[],           // 2.
+            },
+            fragment: Some(wgpu::FragmentState {
+                // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1,                         // 2.
+                mask: !0,                         // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+        });
 
         Self {
             window,
