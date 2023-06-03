3b198ac6478b760d17ac1d1f2c9b93ceafb697f6
trap101 <cheng050929@gmail.com>
2023-06-04 00:30:30 +0100

    attempt index

--- src/lib.rs
+++ src/lib.rs
@@ -262,7 +262,8 @@ impl State {
             render_pass.set_pipeline(&self.render_pipeline); // 2.
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            render_pass.draw_indexed(0..self.num_indices, 0,0..1);
+            // render_pass.draw_indexed(0..self.num_indices, 0,0..1);
+            render_pass.draw(0..3, 0..1)
 
             // 3.
         }
